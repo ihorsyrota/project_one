475ad791e7541e5e51c3a54d7878e779eaadedb6
Ihor Syrota <ihor.syrota@gmail.com>
2018-10-25 02:59:49 +0300

    PO01: Initial files

---
+++ inc/css.inc.php
@@ -0,0 +1,38 @@
+<style>
+* {
+    font-family: Arial;
+    font-size: 12px;
+}
+body, html { 
+    padding:0px; 
+    margin:0px; 
+}
+br {
+    clear: both;
+}
+.nav-list {
+    width: 100%;
+    border: solid 1px #aaa;
+}
+.nav-list tr .apartment-list-cell {
+    border: solid 1px #aaa;
+    border-left: none;
+    border-top: none;
+    width: 16%;
+    padding: 4px;
+}
+.wrapper {
+    padding: 10px;
+}
+.menu {
+    clear: both;
+    background: #ffeeaa;
+}
+.menu li {
+    width: 49%;
+    float: left;
+}
+.menu .active {
+    background: #eee;
+}
+</style>
---
+++ security/settings.php
@@ -0,0 +1,6 @@
+<?php
+$db_user='root';
+$db_pass='';
+$db='dev_x';
+$dsn='mysql:dbname='.$db.';host=localhost';
+?>
